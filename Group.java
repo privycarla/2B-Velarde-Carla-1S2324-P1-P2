4911546eb86fa0d03bebf526fff267280d7753ab
Carla <144458902+privycarla@users.noreply.github.com>
2023-09-09 15:42:55 +0800

    Create Group.java

---
+++ Group.java
@@ -0,0 +1,33 @@
+public class Group {
+
+    public static void main(String[] args) {
+        
+        Order list1 = new Order();
+        list1.setName("Judy Diaga");
+        list1.setFood("Adobo");
+        list1.setDrinks("Coke Zero");
+       
+        Order list2 = new Order();
+        list2.setName("Carla Velarde");
+        list2.setFood("Empanada");
+        list2.setDrinks("Iced coffee");
+
+        Order list3 = new Order();
+        list3.setName("John Punay");
+        list3.setFood("Lechon");
+        list3.setDrinks("Water");
+
+        System.out.println("Order lists");
+        System.out.println("\nCustomer: " +list1.getName ());
+        System.out.println("Food: " +list1.getFood());
+        System.out.println("Drinks: " +list1.getDrinks());
+        System.out.println("\nCustomer: " +list2.getName ());
+        System.out.println("Food: " +list2.getFood());
+        System.out.println("Drinks: " +list2.getDrinks());
+        System.out.println("\nCustomer: " +list3.getName ());
+        System.out.println("Food: " +list3.getFood());
+        System.out.println("Drinks: " +list3.getDrinks());
+
+
+    }
+}
