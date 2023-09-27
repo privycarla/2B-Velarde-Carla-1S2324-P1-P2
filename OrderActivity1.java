cf92b650b1bb6b444a2cc460e89c22d15db0b0ae
Carla <144458902+privycarla@users.noreply.github.com>
2023-09-28 06:50:09 +0800

    OrderActivity1.java

--- OrderActivity1.java
+++ OrderActivity1.java
@@ -1,18 +1,20 @@
 public class Order {
+
    
     private String name;
-    private String food ;
+    private String food;
     private String drinks;
-
+   
     protected Order(String name, String food, String drinks){
-    this.name = name;
-    this.food = food;
-    this.drinks = drinks;
+    
+    this.name = "Name";
+    this.food = "Food";
+    this.drinks = "Drinks";
     }
-
     protected Order () {
-   // System.out.println("Order list");
+        // System.out.println("Order list");
     }   
+   
     protected void setName(String name){
         this.name = name;
     }
@@ -30,5 +32,11 @@ public class Order {
     }
     protected String getDrinks (){
         return this.drinks;
+    }
+ public void display() {
+           
+            System.out.println("Name: " + this.name);
+            System.out.println("Food: " + this.food);
+            System.out.println("Drinks: " + this.drinks + "\n");
     }
 }
